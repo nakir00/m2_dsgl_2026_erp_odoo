9a4433ec013a9e92d3a6cf02ae2ab1236f4a56d9
naby Mbaye <mouhamadnabymbaye@gmail.com>
2026-09-20 17:34:46 +0000

    test: discover module test files

--- custom_addons/pharmacie_management/tests/__init__.py
+++ custom_addons/pharmacie_management/tests/__init__.py
@@ -1,1 +1,7 @@
-from . import test_cash_report
+import importlib
+import pkgutil
+
+
+for module in pkgutil.iter_modules(__path__):
+    if module.name.startswith('test_'):
+        importlib.import_module(f'{__name__}.{module.name}')
